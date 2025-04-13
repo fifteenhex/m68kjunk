570c98fcccaeed03859fef235e3689fa5c04429c
Daniel Palmer <daniel@thingy.jp>
2025-04-13 13:34:49 +0900

    [skip ci] rework the emile stuff a bit
    use the partition id instead of the path so the kernel doesn't
    fail to boot if the scsi config changes

--- mk/mac.mk
+++ mk/mac.mk
@@ -3,6 +3,15 @@ MAC_ROM=machinefiles/mac/quadra.rom
 MAC_PRAM=machinefiles/mac/pram.img
 MAC_CD=bootfiles/maccd.iso
 
+MAC_DISK=bootfiles/disk.mac.raw
+
+# Disk image
+$(MAC_DISK): sfdisk.mac.txt build/buildroot_040/images/rootfs.ext2
+	rm -f %@
+	qemu-img create -f raw $@ 160M
+	/usr/sbin/sfdisk $@ < sfdisk.mac.txt
+	dd if=build/buildroot_040/images/rootfs.ext2 of=$@ seek=1024 bs=1K conv=notrunc
+
 machinefiles/mac:
 	mkdir -p $@
 
@@ -12,7 +21,12 @@ $(MAC_ROM): machinefiles/mac
 $(MAC_PRAM): machinefiles/mac
 	qemu-img create -f raw $@ 256
 
-MAC_CD_CONF=emile_cd.conf
+MAC_CD_CONF=bootfiles/emile_cd.conf
+
+
+$(MAC_CD_CONF):	emile_cd.conf.in bootfiles/disk.mac.raw
+	EMILE_PARTID=`/usr/sbin/blkid -o export bootfiles/disk.mac.raw | grep PTUUID | cut -d "=" -f 2` envsubst < $< > $@
+
 #MAC_CD_APPLEDRIVER=/media/slimboy/coding/m68kjunk/EMILE/build-bbtoolchain/second/appledriver
 MAC_CD_APPLEDRIVER=build/buildroot_040/target/boot/emile/appledriver
 MAC_CD_KERNEL=build/linux_mac/vmlinux.gz
@@ -28,7 +42,8 @@ $(eval $(call create_linux_target,$(LINUX_BUILDDIR_MAC),lc475_defconfig,mac,040,
 QEMU_DEPS_MAC=linux-mac-build \
 	      $(MAC_ROM) \
 	      $(MAC_PRAM) \
-	      $(MAC_CD)
+	      $(MAC_CD) \
+	      $(MAC_DISK)
 
 # Cribbed from https://wiki.qemu.org/Documentation/Platforms/m68k - Running Mac OS 7.1 up to 8.1
 QEMU_CMDLINE_MAC=$(QEMU_BIN) \
@@ -37,7 +52,7 @@ QEMU_CMDLINE_MAC=$(QEMU_BIN) \
         -M q800 \
 	-bios $(MAC_ROM) \
 	-device scsi-hd,scsi-id=1,drive=hd1 \
-	-drive file="build/buildroot_040/images/rootfs.ext2",format=raw,media=disk,if=none,id=hd1 \
+	-drive file=$(MAC_DISK),format=raw,media=disk,if=none,id=hd1 \
 	-drive file=$(MAC_PRAM),format=raw,if=mtd \
 	-device scsi-cd,scsi-id=2,drive=cd0 \
 	-drive file=$(MAC_CD),format=raw,media=cdrom,if=none,id=cd0 \

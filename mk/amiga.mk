511b1ade6700d19181779f0a51f2d6b5784af427
Daniel Palmer <daniel@thingy.jp>
2025-08-04 20:50:45 +0900

    split amiga into 040 and 060

--- mk/amiga.mk
+++ mk/amiga.mk
@@ -1,3 +1,5 @@
-LINUX_BUILDDIR_AMIGA=build/linux_amiga
+LINUX_BUILDDIR_AMIGA_040=build/linux_amiga_040
+LINUX_BUILDDIR_AMIGA_060=build/linux_amiga_060
 
-$(eval $(call create_linux_target,$(LINUX_BUILDDIR_AMIGA),amiga_defconfig,amiga,040,m68k-buildroot-linux-musl-))
+$(eval $(call create_linux_target,$(LINUX_BUILDDIR_AMIGA_040),amiga_defconfig,amiga-040,040,m68k-buildroot-linux-musl-))
+$(eval $(call create_linux_target,$(LINUX_BUILDDIR_AMIGA_060),amiga_defconfig,amiga-060,040,m68k-buildroot-linux-musl-))
